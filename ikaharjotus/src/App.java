14e5c07436322b75b97cc2df68ac829c383c6c04
Mea <amk1005658@student.hamk.fi>
2024-10-25 19:42:36 +0300

    original-code
    tätä muokataan.

---
+++ ikaharjotus/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int age = 18;
+        if (age >=0 && age <= 17)
+        { System.out.println("Olet alaikäinen" ); }
+        else
+        {
+            System.out.println("Olet aikuinen");
+        }
+    }
+}
