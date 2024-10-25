0574b91902638f88cad7bab3c4ad243892751aa7
Mea <amk1005658@student.hamk.fi>
2024-10-25 19:48:40 +0300

    final edit
    lisätty vielä yksi ehto & korjattu alkuperäinen ehto (<= 17) > ( < 18)

--- ikaharjotus/src/App.java
+++ ikaharjotus/src/App.java
@@ -1,13 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int age = 18;
-        if (age >=0 && age <= 17)
+        int age = 65;
+        if (age >=0 && age < 18)
         { System.out.println("Olet alaikäinen" ); }
         else if ( age == 18)
         { System.out.println("Saat ajaa autoa."); }
+        else if (age >= 65)
+        { System.out.println("Olet eläkeläinen."); }
         else
         {
             System.out.println("Olet aikuinen");
         }
+
     }
 }
