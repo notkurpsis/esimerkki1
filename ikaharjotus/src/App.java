482b14770297733349a29f74d63819c3d01b6918
Mea <amk1005658@student.hamk.fi>
2024-10-25 19:44:52 +0300

    added conditions
    lisätty ehtoja (age 18)

--- ikaharjotus/src/App.java
+++ ikaharjotus/src/App.java
@@ -3,6 +3,8 @@ public class App {
         int age = 18;
         if (age >=0 && age <= 17)
         { System.out.println("Olet alaikäinen" ); }
+        else if ( age == 18)
+        { System.out.println("Saat ajaa autoa."); }
         else
         {
             System.out.println("Olet aikuinen");
